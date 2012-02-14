ad08b3592cd59382dc89f43bee5d03fb580c3668
Asa Martin <amartin@bericotechnologies.com>
2012-02-14 07:58:22 -0500

    moved osgi config to spring dm config - still problem getting event manager service by topology service

--- felix-deploy.sh
+++ felix-deploy.sh
@@ -11,4 +11,8 @@ cd ../event-bus-toposervice
 mvn clean -Dmaven.test.skip=true install bundle:bundle
 cp target/*.jar ../../felix-framework-4.0.2/bundle/.
 
-rm -rf ../../felix-framework-4.0.2/felix-cache
+cd ../../felix-framework-4.0.2
+
+rm -rf felix-cache
+
+java -jar bin/felix.jar
