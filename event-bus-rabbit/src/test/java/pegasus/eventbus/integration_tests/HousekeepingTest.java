0e529dcef8194079e0dddea522b3bd4c2e7262b5
Asa Martin <amartin@bericotechnologies.com>
2012-02-15 10:55:48 -0500

    resolving conflict

--- event-bus-rabbit/src/test/java/pegasus/eventbus/integration_tests/HousekeepingTest.java
+++ event-bus-rabbit/src/test/java/pegasus/eventbus/integration_tests/HousekeepingTest.java
@@ -13,64 +13,65 @@ import pegasus.eventbus.testsupport.TestSendEvent;
 
 public class HousekeepingTest extends IntegrationTestBase {
 
-	final Logger LOG                          = LoggerFactory.getLogger(HousekeepingTest.class);
+    final Logger LOG = LoggerFactory.getLogger(HousekeepingTest.class);
 
-	public final class TestHandler implements EventHandler<TestSendEvent> {
-		private final Logger lOG;
+    public final class TestHandler implements EventHandler<TestSendEvent> {
+        private final Logger lOG;
 
-		private TestHandler(Logger lOG) {
-			this.lOG = lOG;
-		}
+        private TestHandler(Logger lOG) {
+            this.lOG = lOG;
+        }
 
-		@SuppressWarnings("unchecked")
+        @SuppressWarnings("unchecked")
         @Override
-		public Class<? extends TestSendEvent>[] getHandledEventTypes() {
-			Class<?>[] types = { TestSendEvent.class };
-			return (Class<? extends TestSendEvent>[]) types;
-		}
+        public Class<? extends TestSendEvent>[] getHandledEventTypes() {
+            Class<?>[] types = { TestSendEvent.class };
+            return (Class<? extends TestSendEvent>[]) types;
+        }
 
-		@Override
-		public EventResult handleEvent(TestSendEvent event) {
-			lOG.info("Received event number: " + event.getCount());
-			return EventResult.Handled;
-		}
-	}
+        @Override
+        public EventResult handleEvent(TestSendEvent event) {
+            lOG.info("Received event number: " + event.getCount());
+            return EventResult.Handled;
+        }
+    }
 
-	@Test 
-	public void publishingAnEventShouldCreateExchangeIfMissing() throws HttpException, IOException{
-		
-		manager.publish(sendEvent);
+    @Test
+    public void publishingAnEventShouldCreateExchangeIfMissing() throws HttpException, IOException {
 
-		assertExchangeExists();
-	}
+        manager.publish(sendEvent);
 
-	@Test 
-	public void publishingAnEventShouldNotCreateAnyQueues() throws HttpException, IOException{
-		
-		manager.publish(sendEvent);
+        assertExchangeExists();
+    }
 
-		assertQueueDoesNotExists();
-	}
-	
-	@Test @Ignore("This test has to be run manually.  Its purpose it to verify that publish and subscribe actions can recover from connection failures.  See comments.")
-	//To correctly run this test, start test, then stop the RabbitMq server and restart it.  Ensure that log shows all events were received in numeric order.
-	//Unplugging and plugging network cable will not correctly test this as explained here: http://lists.rabbitmq.com/pipermail/rabbitmq-discuss/2011-September/015329.html 
-	public void connectivityTest() throws InterruptedException{
+    @Test
+    public void publishingAnEventShouldNotCreateAnyQueues() throws HttpException, IOException {
 
-		manager.subscribe(new TestHandler(LOG));
-		
-		int count = 0;
-		while(true){
-			try{
-				TestSendEvent event = new TestSendEvent();
-				event.setCount(count);
-				manager.publish(event);
-				count++;
-			} catch (Exception e) {
-				//LOG.error("Error sending event " + count + ": " + e.getMessage(), e);
-			}
-			Thread.sleep(1000);
-		}
-	}
-	
+        manager.publish(sendEvent);
+
+        assertQueueDoesNotExists();
+    }
+
+    @Test
+    @Ignore("This test has to be run manually.  Its purpose it to verify that publish and subscribe actions can recover from connection failures.  See comments.")
+    // To correctly run this test, start test, then stop the RabbitMq server and restart it. Ensure that log shows all events were received in numeric order.
+    // Unplugging and plugging network cable will not correctly test this as explained here: http://lists.rabbitmq.com/pipermail/rabbitmq-discuss/2011-September/015329.html
+    public void connectivityTest() throws InterruptedException {
+
+        manager.subscribe(new TestHandler(LOG));
+
+        int count = 0;
+        while (true) {
+            try {
+                TestSendEvent event = new TestSendEvent();
+                event.setCount(count);
+                manager.publish(event);
+                count++;
+            } catch (Exception e) {
+                // LOG.error("Error sending event " + count + ": " + e.getMessage(), e);
+            }
+            Thread.sleep(1000);
+        }
+    }
+
 }
