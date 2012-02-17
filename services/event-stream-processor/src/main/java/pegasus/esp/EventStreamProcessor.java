4bcb2df3bdb2a64e485bbfcace304a7e5c58fe91
Bruce Israel <bisrael@bericotechnologies.com>
2012-02-17 17:39:00 -0500

    event bus integration and code cleanup

--- services/event-stream-processor/src/main/java/pegasus/esp/EventStreamProcessor.java
+++ services/event-stream-processor/src/main/java/pegasus/esp/EventStreamProcessor.java
@@ -5,6 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 import pegasus.eventbus.client.Envelope;
 import pegasus.eventbus.client.EnvelopeHandler;
+import pegasus.eventbus.client.EventHandler;
 import pegasus.eventbus.client.EventManager;
 import pegasus.eventbus.client.EventResult;
 import pegasus.eventbus.client.Subscription;
@@ -12,6 +13,7 @@ import pegasus.eventbus.client.SubscriptionToken;
 
 import com.espertech.esper.client.Configuration;
 import com.espertech.esper.client.EPAdministrator;
+import com.espertech.esper.client.EPException;
 import com.espertech.esper.client.EPRuntime;
 import com.espertech.esper.client.EPServiceProvider;
 import com.espertech.esper.client.EPServiceProviderManager;
@@ -20,160 +22,184 @@ import com.espertech.esper.client.EventBean;
 import com.espertech.esper.client.UpdateListener;
 import com.google.common.annotations.VisibleForTesting;
 
-class EventStreamProcessor {
+public class EventStreamProcessor {
 
-    private EventManager eventbus;
+	private EventManager eventbus;
 
-    private static final Log logger = LogFactory.getLog(EventStreamProcessor.class);
+	private static final Log logger = LogFactory.getLog(EventStreamProcessor.class);
 
-    class EnvelopeListener implements UpdateListener {
+	class EnvelopeListener implements UpdateListener {
 
-        public EnvelopeListener(EventMonitor monitor) {
-            super();
-            this.monitor = monitor;
-        }
+		public EnvelopeListener(EventMonitor monitor) {
+			super();
+			this.monitor = monitor;
+		}
 
-        EventMonitor monitor;
+		EventMonitor monitor;
 
-        @Override
-        public void update(EventBean[] newEvents, EventBean[] oldEvents) {
-            try {
-                for (EventBean eventBean : newEvents) {
-                    logger.debug(Utils.beanString(eventBean) + "==> " + monitor + ".receive(" + ")");
-                    processResult(monitor.receive(eventBean));
-                }
-//                resultingEvent = monitor.handle(newEvents, oldEvents);
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
+		@Override
+		public void update(EventBean[] newEvents, EventBean[] oldEvents) {
+			try {
+				for (EventBean eventBean : newEvents) {
+					logger.debug(Utils.beanString(eventBean) + "==> " + monitor + ".receive(" + ")");
+					processResult(monitor.receive(eventBean));
+				}
+				//                resultingEvent = monitor.handle(newEvents, oldEvents);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
 
-        }
+		}
 
-        private void processResult(InferredEvent resultingEvent) {
-            if (resultingEvent == null) {
-                return;
-            }
+		private void processResult(InferredEvent resultingEvent) {
+			if (resultingEvent == null) {
+				return;
+			}
 
-            EPRuntime epRuntime = epService.getEPRuntime();
-            if (resultingEvent instanceof InferredEventList) {
-                for (InferredEvent ev : ((InferredEventList) resultingEvent).getList()) {
-                    logger.debug(monitor + " SENDS* " + ev);
-                    epRuntime.sendEvent(ev);
-                }
-            } else {
-                logger.debug(monitor + " SENDS " + resultingEvent);
-                epRuntime.sendEvent(resultingEvent);
-            }
-        }
-    }
+			EPRuntime epRuntime = epService.getEPRuntime();
+			if (resultingEvent instanceof InferredEventList) {
+				for (InferredEvent ev : ((InferredEventList) resultingEvent).getList()) {
+					logger.debug(monitor + " SENDS* " + ev);
+					epRuntime.sendEvent(ev);
+				}
+			} else {
+				logger.debug(monitor + " SENDS " + resultingEvent);
+				epRuntime.sendEvent(resultingEvent);
+			}
+		}
+	}
 
-    class EventbusListener implements EnvelopeHandler {
+	class EventbusListener implements EnvelopeHandler {
 
-        private EventStreamProcessor eventStreamProcessor;
+		private EventStreamProcessor eventStreamProcessor;
 
-        public EventbusListener(EventStreamProcessor eventStreamProcessor) {
-            this.eventStreamProcessor = eventStreamProcessor;
-        }
+		public EventbusListener(EventStreamProcessor eventStreamProcessor) {
+			this.eventStreamProcessor = eventStreamProcessor;
+		}
 
-        @Override
-        public EventResult handleEnvelope(Envelope envelope) {
-            sendEvent(envelope);
-            return EventResult.Handled;
-        }
+		@Override
+		public EventResult handleEnvelope(Envelope envelope) {
+			eventStreamProcessor.sendEvent(envelope);
+			return EventResult.Handled;
+		}
 
 		@Override
 		public String getEventSetName() {
 			return "ALL";
 		}
 
-    }
+	}
 
-    // TODO: consider making this a constructor parameter to allow for multiple instances
-    public static final String engineURI = "EventStreamProcessor";
+	// TODO: consider making this a constructor parameter to allow for multiple instances
+	public static final String engineURI = "EventStreamProcessor";
 
-    private EPServiceProvider epService;
+	private EPServiceProvider epService;
 
-    private SubscriptionToken token;
+	private SubscriptionToken token;
 
-    private EventMonitorRepository repository;
+	private EventMonitorRepository repository;
 
-    public EventStreamProcessor(EventMonitorRepository rep) {
-        this.repository = rep;
-        epService = createEventProcessor();
-        rep.registerWith(this);
-    }
+	public EventStreamProcessor(EventMonitorRepository rep) {
+		this();
+		setRepository(rep);
+	}
 
-    private EPServiceProvider createEventProcessor() {
-        Configuration configuration = new Configuration();
-        configuration.addEventType("Envelope", Envelope.class);
-        configuration.addEventType("InferredEvent", InferredEvent.class);
-        EPServiceProvider epService = EPServiceProviderManager.getProvider(engineURI , configuration);
-        epService.initialize();
-        return epService;
-    }
+	public void setRepository(EventMonitorRepository repository) {
+		this.repository = repository;
+		repository.registerWith(this);
+	}
 
-    public void attachToEventBus() {
-        EnvelopeHandler envelopeHandler = new EventbusListener(this);
-        Subscription subscription = new Subscription(envelopeHandler);
-        token = eventbus.subscribe(subscription);
-    }
+	public EventStreamProcessor() {
+		epService = createEventProcessor();
+	}
 
-    public void detachFromEventBus() {
-        eventbus.unsubscribe(token);
-        token = null;
-    }
+	private EPServiceProvider createEventProcessor() {
+		Configuration configuration = new Configuration();
+		configuration.addEventType("Envelope", Envelope.class);
+		configuration.addEventType("InferredEvent", InferredEvent.class);
+		EPServiceProvider epService = EPServiceProviderManager.getProvider(engineURI , configuration);
+		epService.initialize();
+		return epService;
+	}
 
-    public void watchFor(EventMonitor monitor) {
-        monitor.registerPatterns(this);
-    }
+	public void attachToEventBus(EventManager eventbus) {
+		if (eventbus != null) {
+			detachFromEventBus();
+		}
+		this.eventbus = eventbus;
+		EnvelopeHandler envelopeHandler = new EventbusListener(this);
+		Subscription subscription = new Subscription(envelopeHandler);
+//        EventHandler<?> evtmp = null;
+//		Subscription subscription = new Subscription(evtmp );
+		token = eventbus.subscribe(subscription);
+	}
 
-    @VisibleForTesting
-    public void sendEvent(Envelope envelope) {
-        logger.debug(" --> Event: " + envelope);
-        epService.getEPRuntime().sendEvent(envelope);
+	public void detachFromEventBus() {
+		if (eventbus != null) {
+			eventbus.unsubscribe(token);
+			token = null;
+			eventbus = null;
+		}
+	}
 
-    }
+	public void watchFor(EventMonitor monitor) {
+		monitor.registerPatterns(this);
+	}
 
-    public void monitor(boolean isEPL, String pattern, EventMonitor monitor) {
-        EPAdministrator administrator = epService.getEPAdministrator();
-        EPStatement stmt;
-        logger.debug("Creating " + (isEPL ? "EPL" : "pattern") + ": " + pattern);
-        if (isEPL) {
-            stmt = administrator.createEPL(pattern);
-        } else {
-            stmt = administrator.createPattern(pattern);
-        }
-        if (monitor != null) {
-            stmt.addListener(new EnvelopeListener(monitor));
-        }
-        if (monitor == null) {
-            UpdateListener intermediatePrinter = createIntermediatePrinter(pattern);
-            if (intermediatePrinter != null) {
-                stmt.addListener(intermediatePrinter);
-            }
-        }
-    }
+	@VisibleForTesting
+	public void sendEvent(Envelope envelope) {
+		logger.debug(" --> Event: " + envelope);
+		epService.getEPRuntime().sendEvent(envelope);
 
-    private UpdateListener createIntermediatePrinter(final String pattern) {
-        boolean watchInt = true;
-        if (! watchInt) { return null; }
+	}
 
-        String displ = pattern;
-        String insert = "insert into ";
-        if (pattern.startsWith(insert)) {
-            displ = pattern.substring(insert.length()).split("\\W")[0];
-        }
-        final String fdispl = displ;
-        UpdateListener ip = new UpdateListener() {
-            @Override
-            public void update(EventBean[] newEvents, EventBean[] oldEvents) {
-                for (EventBean eventBean : newEvents) {
-                    logger.debug("!!!! " + fdispl + ": " + Utils.beanString(eventBean));
-                }
-            }
-        };
+	public void monitor(boolean isEPL, String pattern, EventMonitor monitor) {
+		EPAdministrator administrator = epService.getEPAdministrator();
+		EPStatement stmt;
+		logger.debug("Creating " + (isEPL ? "EPL" : "pattern") + ": " + pattern);
+		try {
+			if (isEPL) {
+				stmt = administrator.createEPL(pattern);
+			} else {
+				stmt = administrator.createPattern(pattern);
+			}
+		} catch (EPException e) {
+			System.err.println("Error creating " +
+					(isEPL ? "EPL" : "Pattern") + " statement: " + pattern);
+			e.printStackTrace();
+			throw e;
+		}
+		if (monitor != null) {
+			stmt.addListener(new EnvelopeListener(monitor));
+		}
+		if (monitor == null) {
+			UpdateListener intermediatePrinter = createIntermediatePrinter(pattern);
+			if (intermediatePrinter != null) {
+				stmt.addListener(intermediatePrinter);
+			}
+		}
+	}
 
-        return ip;
-    }
+	private UpdateListener createIntermediatePrinter(final String pattern) {
+		boolean watchInt = true;
+		if (! watchInt) { return null; }
+
+		String displ = pattern;
+		String insert = "insert into ";
+		if (pattern.startsWith(insert)) {
+			displ = pattern.substring(insert.length()).split("\\W")[0];
+		}
+		final String fdispl = displ;
+		UpdateListener ip = new UpdateListener() {
+			@Override
+			public void update(EventBean[] newEvents, EventBean[] oldEvents) {
+				for (EventBean eventBean : newEvents) {
+					logger.debug("!!!! " + fdispl + ": " + Utils.beanString(eventBean));
+				}
+			}
+		};
+
+		return ip;
+	}
 }
