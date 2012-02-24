2b4ca7076600789ec6c10dacb952bb58b121d206
Richard Clayton <rclayton@bericotechnologies.com>
2012-02-24 15:51:47 -0500

    Updated auditor to use the new version of the EventManager

--- services/security/auditor/src/main/java/pegasus/eventbus/services/auditor/AuditorService.java
+++ services/security/auditor/src/main/java/pegasus/eventbus/services/auditor/AuditorService.java
@@ -1,41 +1,101 @@
 package pegasus.eventbus.services.auditor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.HashMap;
+import java.util.Map;
 
 import pegasus.eventbus.amqp.AmqpEventManager;
-import pegasus.eventbus.amqp.Configuration;
+import pegasus.eventbus.amqp.AmqpConfiguration;
 import pegasus.eventbus.amqp.ConnectionParameters;
 import pegasus.eventbus.client.EventManager;
+import pegasus.eventbus.client.SubscriptionToken;
 
 /**
- * 
+ * Service that logs (Audits) Event Sets using the SLF4J logging facade.
  * @author Richard Clayton (Berico Technologies)
  */
 public class AuditorService 
 {
-	private static final Logger LOG = LoggerFactory.getLogger(AuditorService.class);
-	
 	EventManager eventManager = null;
+	Map<String, SubscriptionToken> tokens = new HashMap<String, SubscriptionToken>();
 	
-	public AuditorService(EventManager eventManager){
+	/**
+	 * Initialize the Auditor Service
+	 * @param eventManager EventManager instance
+	 * @param eventSets Array of Event Sets to log.
+	 */
+	public AuditorService(EventManager eventManager, String[] eventSets){
 		
 		this.eventManager = eventManager;
 		
-		this.eventManager.subscribe(new AuditEventHandler());
-		
-		this.eventManager.publish("Hi Mom!");
+		for(String eventSet : eventSets){
+			
+			this.eventManager.subscribe(new Auditor(eventSet));
+		}
 	}
 	
+	/**
+	 * Audit the supplied Event Set.
+	 * @param eventSet Name of the Event Set.
+	 */
+	public void auditEventSet(String eventSet){
+		
+		tokens.put(
+			eventSet,
+			this.eventManager.subscribe(
+				new Auditor(eventSet)));
+	}
 	
+	/**
+	 * Audit the supplied Event Set.
+	 * @param eventSet Name of the Event Set.
+	 * @param logname Identifier for the Log.
+	 */
+	public void auditEventSet(String eventSet, String logname){
+		
+		tokens.put(
+			eventSet,
+			this.eventManager.subscribe(
+				new Auditor(eventSet, logname)));
+	}
+	
+	/**
+	 * Stop an Active Audit (if the event set is currently being watched).
+	 * @param eventSet Event Set whose audit should be stopped.
+	 */
+	public void removeAudit(String eventSet){
+		
+		if(tokens.containsKey(eventSet)){
+			
+			this.eventManager.unsubscribe(tokens.get(eventSet));
+		}
+	}
+	
+	/**
+	 * Shutdown (unregistering all auditors).
+	 */
+	public void shutdown(){
+		
+		for(SubscriptionToken token : tokens.values()){
+			
+			this.eventManager.unsubscribe(token);
+		}
+	}
+	
+	/**
+	 * Start up the auditor service.
+	 * @param args Named Event Sets to Capture to the Log.
+	 */
     public static void main( String[] args )
     {	
-    	Configuration config = Configuration.getDefault(
-    			"Orion:AuditorService", 
+    	
+    	AmqpConfiguration config = AmqpConfiguration.getDefault(
+    			"AuditorService", 
     			new ConnectionParameters("amqp://guest:guest@localhost:5672/"));
     	
-    	new AuditorService(new AmqpEventManager(config));
+    	EventManager em = new AmqpEventManager(config);
     	
-        System.out.println( "Hello World!" );
+    	em.start();
+    	
+    	new AuditorService(em, args);
     }
 }
