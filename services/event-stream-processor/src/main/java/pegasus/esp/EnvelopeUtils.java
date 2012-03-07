9b73bc4a2449da76d5e66eee63446222caba5ccc
Bruce Israel <bisrael@bericotechnologies.com>
2012-03-06 21:51:09 -0500

    Readable envelope formatting

--- services/event-stream-processor/src/main/java/pegasus/esp/EnvelopeUtils.java
+++ services/event-stream-processor/src/main/java/pegasus/esp/EnvelopeUtils.java
@@ -1,11 +1,15 @@
 package pegasus.esp;
 
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import pegasus.eventbus.client.Envelope;
 
 import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class EnvelopeUtils {
 
@@ -22,11 +26,11 @@ public class EnvelopeUtils {
 	 */
 	public static UUID symIdToRealId(String idsymbol) {
 	    UUID id = null;
-	
+
 	    if (idsymbol != null) {
 	        id = idMappings.get(idsymbol);
 	    }
-	
+
 	    if (id == null) {
 	        id = UUID.randomUUID();
 	        idMappings.put(idsymbol, id);
@@ -47,4 +51,50 @@ public class EnvelopeUtils {
 	    return e;
 	}
 
+	private final static Gson gson_pp = new GsonBuilder().setPrettyPrinting().create();
+	private final static Gson gson = new Gson();
+
+	public static String toFormattedJson(String line) {
+        HashMap map = gson.fromJson(line, HashMap.class);
+        String mapstr = gson.toJson(map);
+        return mapstr;
+    }
+
+	public static String toJson(Envelope env) {
+	    return gson.toJson(env, Envelope.class);
+	}
+
+	public static String toPrettyJson(Envelope env) {
+	    return gson_pp.toJson(env, Envelope.class);
+	}
+
+	public static String envelopeToReadableJson(Envelope env) {
+		byte[] body = env.getBody();
+		String bodyJson = "";
+        try {
+            bodyJson = new String(body, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String bodyformatted = toFormattedJson(bodyJson);
+        JsonString json = new JsonString().start()
+				.add("EVENT_TYPE", env.getEventType())
+				.add("REPLYTO", env.getReplyTo())
+				.add("TOPIC", env.getTopic())
+				.add("ID", env.getId())
+				.add("CORRELATION_ID", env.getCorrelationId())
+				.add("TIMESTAMP", env.getTimestamp())
+				.add("HEADERS", env.getHeaders())
+				.add("BODY_SIZE", body.length)
+				.add("BODY_SRC", body)
+				.add("BODY_JSON", bodyJson)
+				.add("BODY", bodyformatted)
+				;
+		return json.end().toString();
+	}
+
+    public static Envelope fromJson(String line) {
+        Envelope envelope = gson.fromJson(line, Envelope.class);
+        return envelope;
+    }
 }
