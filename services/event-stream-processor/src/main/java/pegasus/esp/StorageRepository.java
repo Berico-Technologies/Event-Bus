9f62a93710902d2a39e57a8e961f7898b1f0fc47
Bruce Israel <bisrael@bericotechnologies.com>
2012-03-06 22:04:53 -0500

    added constructors and toString()

--- services/event-stream-processor/src/main/java/pegasus/esp/StorageRepository.java
+++ services/event-stream-processor/src/main/java/pegasus/esp/StorageRepository.java
@@ -9,6 +9,22 @@ public class StorageRepository implements EventMonitorRepository {
     List<EventMonitor> monitors = Lists.newArrayList();
     private EventStreamProcessor eventStreamProcessor = null;
 
+    public StorageRepository() {
+    }
+
+    // TODO: extend this to take a list
+    public StorageRepository(EventMonitor monitor) {
+        addMonitor(monitor);
+    }
+
+    public List<EventMonitor> getMonitors() {
+        return monitors;
+    }
+
+    public void setMonitors(List<EventMonitor> monitors) {
+        this.monitors = monitors;
+    }
+
     public EventMonitorRepository addMonitor(EventMonitor monitor) {
         monitors.add(monitor);
         if (eventStreamProcessor != null) {
@@ -24,4 +40,17 @@ public class StorageRepository implements EventMonitorRepository {
             eventStreamProcessor.watchFor(monitor);
         }
     }
+
+    @Override
+    public String toString() {
+        StringBuffer sb =
+                new StringBuffer(this.getClass().getSimpleName() + "#" + monitors.size() + "[");
+        String sep = "";
+        for (EventMonitor monitor : monitors) {
+            sb.append(sep + monitor);
+            sep = ", ";
+        }
+        return sb.append("]").toString();
+    }
+
 }
