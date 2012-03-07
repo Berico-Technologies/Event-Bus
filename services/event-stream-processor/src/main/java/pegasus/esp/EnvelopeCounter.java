20d516cd869f1bf3c14a9c92983270bf37e3421c
Bruce Israel <bisrael@bericotechnologies.com>
2012-03-07 11:41:31 -0500

    integrated time ranges into Envelope Counter

--- services/event-stream-processor/src/main/java/pegasus/esp/EnvelopeCounter.java
+++ services/event-stream-processor/src/main/java/pegasus/esp/EnvelopeCounter.java
@@ -1,4 +1,4 @@
-package pegasus.esp;
+≈ßpackage pegasus.esp;
 
 import java.util.Collection;
 import java.util.Date;
@@ -9,6 +9,7 @@ import java.util.Set;
 
 import org.joda.time.DateTime;
 
+import pegasus.esp.data.ActiveRange;
 import pegasus.esp.data.ValueStreams;
 import pegasus.eventbus.client.Envelope;
 
@@ -18,29 +19,57 @@ import com.google.common.collect.Maps;
 
 public class EnvelopeCounter extends EventMonitor {
 
+    int[] defaultperiods = {
+            ValueStreams.seconds(30),
+            ValueStreams.minutes(1),
+            ValueStreams.minutes(5),
+            ValueStreams.hours(1)
+            };
+
     public interface EnvelopeRetriever {
         public String retrieve(Envelope e);
         public String key();
+        public int[] periods();
+        public int getValue(String type, Envelope env);
     }
 
     Collection<EnvelopeRetriever> counters = Lists.newArrayList();
-    private Map<String, ValueStreams> dc = Maps.newHashMap();
+    private Map<String, ValueStreams> streamsMap = Maps.newHashMap();
 
     public EnvelopeCounter() {
 
         counters.add(new EnvelopeRetriever() {
             public String retrieve(Envelope e) { return e.getEventType(); }
             public String key() { return "EventType"; }
+            public int[] periods() { return defaultperiods; }
+            public int getValue(String type, Envelope env) { return 1; }
         });
 
         counters.add(new EnvelopeRetriever() {
-            public String retrieve(Envelope e) { return e.getTopic(); }
-            public String key() { return "Topic"; }
+            public String retrieve(Envelope e) { return e.getEventType(); }
+            public String key() { return "Total Body Length by Type"; }
+            public int[] periods() { return defaultperiods; }
+            public int getValue(String type, Envelope env) { return env.getBody().length; }
         });
+
+        boolean countTopics = false;
+
+        if (countTopics) {
+            counters.add(new EnvelopeRetriever() {
+                public String retrieve(Envelope e) { return e.getTopic(); }
+                public String key() { return "Topic"; }
+                public int[] periods() { return defaultperiods; }
+                public int getValue(String type, Envelope env) { return 1; }
+            });
+        }
 
         for (EnvelopeRetriever envelopeRetriever : counters) {
             String type = envelopeRetriever.key();
-            dc.put(type, new ValueStreams(type));
+            ValueStreams valueStreams = new ValueStreams(type);
+            streamsMap.put(type, valueStreams);
+            for (int per : envelopeRetriever.periods()) {
+                valueStreams.addPeriod(per);
+            }
         }
     }
 
@@ -53,10 +82,19 @@ public class EnvelopeCounter extends EventMonitor {
 
     public void dumpFreq(String type) {
         System.out.println(type + ":");
-        ValueStreams stream = dc.get(type);
-        for (String cat : stream.getActiveRanges()) {
+        ValueStreams stream = streamsMap.get(type);
+        for (String category : stream.getActiveRanges()) {
+            System.out.println(String.format("\nTopN Label: %s", category));
             for (String item : stream.getValues()) {
-                System.out.println("  " + cat + "(" + item + ")=" + stream.getActiveRange(cat, item).getTotal());
+                ActiveRange activeRange = stream.getActiveRange(category, item);
+                int total = activeRange.getTotal();
+                int trend = activeRange.getTrend();
+                String desc = activeRange.getTrendDesc();
+                System.out.println(String.format("  Trend Label: %s", item));
+                System.out.println(String.format("  Trend Value: %s", total));
+                System.out.println(String.format("  Trend info: %s", desc));
+                System.out.println(String.format("  Trend change: %s", trend));
+                System.out.println();
             }
         }
         System.out.println();
@@ -65,18 +103,19 @@ public class EnvelopeCounter extends EventMonitor {
     @Override
     public InferredEvent receive(EventBean eventBean) {
         Envelope env = (Envelope) eventBean.get("env");
-        recordOccurrences(env);
+        recordValues(env);
         return null;
     }
 
-    private void recordOccurrences(Envelope env) {
+    private void recordValues(Envelope env) {
         for (EnvelopeRetriever counter : counters) {
             String type = counter.key();
             String item = counter.retrieve(env);
-            ValueStreams valueStreams = dc.get(type);
+            int value = counter.getValue(type, env);
+            ValueStreams valueStreams = streamsMap.get(type);
             Date timestamp = env.getTimestamp();
             long time = timestamp.getTime();
-            valueStreams.addValue(item, time, 1);
+            valueStreams.addValue(item, time, value);
         }
     }
 
