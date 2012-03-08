1c385638ac0936180c2372ed685e8cd690742773
John Ruiz <jruiz@johnruiz.com>
2012-03-08 18:06:30 -0500

    daily checkin

--- event-bus-rabbit/src/main/dotnet/amqp/IAmqpMessageBus.cs
+++ event-bus-rabbit/src/main/dotnet/amqp/IAmqpMessageBus.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace event_bus_rabbit
+namespace pegasus.eventbus.amqp
 {
-    public class Class1
+    public interface IAmqpMessageBus
     {
     }
 }
---
+++ event-bus-rabbit/src/main/dotnet/amqp/ITopologyService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pegasus.eventbus.amqp
+{
+    public interface ITopologyService
+    {
+        void Start();
+
+        void Close();
+
+        RoutingInfo GetRoutingInfoForEventOfType(Type evType);
+    }
+}
