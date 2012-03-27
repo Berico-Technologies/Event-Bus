72620a139df98766cca7b2e25491abb1f0063cac
Ken Baltrinic <kbaltrinic@bericotechnologies.com>
2012-03-27 09:02:10 -0400

    Switch to use canonicalName for logger name vice name as this is more appropriate.  Also adds some missing TODO comments.

--- event-bus-rabbit/src/main/java/pegasus/eventbus/amqp/EventEnvelopeHandler.java
+++ event-bus-rabbit/src/main/java/pegasus/eventbus/amqp/EventEnvelopeHandler.java
@@ -31,6 +31,7 @@ class EventEnvelopeHandler implements EnvelopeHandler {
 
         this.amqpEventManager = amqpEventManager;
 
+        //TODO: PEGA-727 Need to add tests to assert that this logger name is always valid (i.e. queue names with . and any other illegal chars are correctly mangled.)
         LOG = LoggerFactory.getLogger(String.format("%s$>%s", this.getClass().getCanonicalName(), eventHandler.getClass().getName().replace('.', '_')));
 
         LOG.trace("EventEnvelopeHandler instantiated for EventHandler of type {}", eventHandler.getClass().getName());
